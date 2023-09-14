6f2a31e82ba0be6c63b1c796d0601b55d8e80ebc
Timothy Ademola <timife007@gmail.com>
2023-09-14 13:13:48 +0100

    book update integration test

--- src/test/java/com/timife/prodatabase/dao/impl/BookDaoImplIntegrationTest.java
+++ src/test/java/com/timife/prodatabase/dao/impl/BookDaoImplIntegrationTest.java
@@ -60,7 +60,22 @@ public class BookDaoImplIntegrationTest {
 
         List<Book> result = underTest.find();
         assertThat(result).hasSize(3).containsExactly(bookA, bookB, bookC);
+    }
 
+    @Test
+    public void testThatBookCanBeUpdated(){
+        Author author = TestDataUtil.createTestAuthorA();
+        authorDao.create(author);
+
+        Book bookA = TestDataUtil.createTestBookA();
+        bookA.setAuthorId(author.getId());
+        underTest.create(bookA);
+
+        bookA.setTitle("UPDATED");
+        underTest.update(bookA.getIsbn(), bookA);
+        Optional<Book> result = underTest.findOne(bookA.getIsbn());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(bookA);
 
     }
 }
