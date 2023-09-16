e65ea54e96cc63bc520274cf43d0b2f02305e585
Timothy Ademola <timife007@gmail.com>
2023-09-16 18:16:03 +0100

    i don't need to set author again

--- src/test/java/com/timife/prodatabase/repositories/BookRepositoryIntegrationTest.java
+++ src/test/java/com/timife/prodatabase/repositories/BookRepositoryIntegrationTest.java
@@ -43,15 +43,11 @@ public class BookRepositoryIntegrationTest {
     public void testThatMultipleBooksCanBeCreatedAndRecalled(){
         Author author = TestDataUtil.createTestAuthorA();
         Book bookA = TestDataUtil.createTestBookA(author);
-        bookA.setAuthor(author);
         underTest.save(bookA);
         Book bookB = TestDataUtil.createTestBookB(author);
-        bookB.setAuthor(author);
         underTest.save(bookB);
         Book bookC = TestDataUtil.createTestBookC(author);
-        bookC.setAuthor(author);
         underTest.save(bookC);
-
         Iterable<Book> result = underTest.findAll();
         assertThat(result).hasSize(3).containsExactly(bookA, bookB, bookC);
     }
