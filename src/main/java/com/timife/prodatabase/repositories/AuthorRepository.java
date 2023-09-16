09e644b4c03b1f0aca19fc366fec15f78b22a15a
Timothy Ademola <timife007@gmail.com>
2023-09-16 18:06:15 +0100

    test creation of book and author

---
+++ src/main/java/com/timife/prodatabase/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.timife.prodatabase.repositories;
+
+import com.timife.prodatabase.domain.Author;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+}
