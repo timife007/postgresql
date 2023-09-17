49c8cf82e642ff9a9514347ce74f109e1d098fbf
Timothy Ademola <timife007@gmail.com>
2023-09-17 05:55:10 +0100

    author service linking repository with presentation impl

--- src/main/java/com/timife/prodatabase/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/timife/prodatabase/services/impl/AuthorServiceImpl.java
@@ -1,6 +1,18 @@
 package com.timife.prodatabase.services.impl;
 
+import com.timife.prodatabase.domain.entities.AuthorEntity;
+import com.timife.prodatabase.repositories.AuthorRepository;
 import com.timife.prodatabase.services.AuthorService;
 
 public class AuthorServiceImpl implements AuthorService {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository){
+        this.authorRepository = authorRepository;
+    }
+    @Override
+    public AuthorEntity createAuthor(AuthorEntity author) {
+        return authorRepository.save(author);
+    }
 }
