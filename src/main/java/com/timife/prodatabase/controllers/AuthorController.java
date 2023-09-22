757777c865e96c994721ab61b137d406ec6b9864
Timothy Ademola <timife007@gmail.com>
2023-09-23 00:24:20 +0100

    delete item in authors

--- src/main/java/com/timife/prodatabase/controllers/AuthorController.java
+++ src/main/java/com/timife/prodatabase/controllers/AuthorController.java
@@ -90,4 +90,11 @@ public class AuthorController {
         return new ResponseEntity<>(
                 authorMapper.mapTo(updatedAuthor), HttpStatus.OK);
     }
+
+    @DeleteMapping(path = "/authors/{id}")
+    public ResponseEntity deleteAuthor(@PathVariable("id") Long id){
+        authorService.delete(id);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+
+    }
 }
